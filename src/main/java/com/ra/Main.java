1ea42ffa9a9360bd458796689b606e4c0ee6bec2
hoangthuy99 <89949702+hoangthuy99@users.noreply.github.com>
2025-11-27 11:48:36 +0700

    Save Main.java

--- src/main/java/com/ra/Main.java
+++ src/main/java/com/ra/Main.java
@@ -1,8 +1,6 @@
 package com.ra;
 
-import com.ra.Utils.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+
 
 public class Main {
     public static void main(String[] args) {
