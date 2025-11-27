9c6d88b8146be3bdc9391399b0e70f0a631185ed
TramNguyen114 <ngoctramat@gmail.com>
2025-11-27 10:30:19 +0700

    tram 27/11/2025

--- src/main/java/com/ra/Main.java
+++ src/main/java/com/ra/Main.java
@@ -1,24 +1,9 @@
 package com.ra;
 
-
-import com.ra.Utils.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import com.ra.View.login.LoginScreen;
 
 public class Main {
     public static void main(String[] args) {
-     //TODO:Kết nối với hibernate
-        Transaction transaction = null;
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            transaction = session.beginTransaction();
-            System.out.println("Kết nối thành công đến cơ sở dữ liệu!");
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
+       
     }
 }
