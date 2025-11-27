ffff046db6e7e70dd1726560a3de43b0aef6d401
hunterhyt012-collab <hunterhyt012@gmail.com>
2025-11-27 09:59:06 +0700

    Huynh 27/11

--- src/main/java/com/ra/DTO/request/UserRequest.java
+++ src/main/java/com/ra/DTO/request/UserRequest.java
@@ -18,7 +18,6 @@ public class UserRequest {
     @NotBlank(message = "Username is required")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
-    private String userCode;
 
     @NotBlank(message = "Password is required")
     @Size(min = 6, max = 100, message = "Password must be at least 6 characters long")
